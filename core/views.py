566799721698652d1882a00f65c6e474ef6ec29b
ISLOMJON MASALIEV <imasaliev@gmail.com>
2023-06-28 20:11:26 -0500

    t

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class HomeView(generic.TemplateView):
+	"""
+    Website home page.
+
+    **Template:**
+
+    :template:`core/index.html`
+    """
+	template_name = "core/index.html"
